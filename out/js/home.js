976c1049c0071ee5b400476efd2bd592f850fe1f
MarceloAmp27 <marceloamellopaixao@gmail.com>
2023-01-26 20:52:37 -0300

    v1.0.2

--- out/js/home.js
+++ out/js/home.js
@@ -10,15 +10,23 @@ function alertMsg() {
     window.location.href = "../html/login.html";
   }
 }
-
 function page1() {
-  window.location.href = "./projects/login/login.html";
+  window.location.href =
+    "https://drive.google.com/drive/folders/1atbr6iSrX7eJs4LR5mz4fE1p_EJgfQPM";
 }
 
 function page2() {
-  window.location.href = "./projects/signup/sign-up.html";
+  window.location.href = "./projects/login/login.html";
 }
 
 function page3() {
+  window.location.href = "./projects/signup/sign-up.html";
+}
+
+function page4() {
   window.location.href = "./projects/rocketseat/habits/index.html";
+}
+
+function page5() {
+  window.location.href = "./projects/rocketseat/social-tree/index.html";
 }
--- out/src/index.js
+++ out/src/index.js
@@ -1,35 +1,35 @@
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  limit,
-  onSnapshot,
-  setDoc,
-  updateDoc,
-  doc,
-  serverTimestamp,
-} from "firebase/firestore";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import { getPerformance } from "firebase/performance";
+// import { initializeApp } from "firebase/app";
+// import {
+//   getAuth,
+//   onAuthStateChanged,
+//   GoogleAuthProvider,
+//   signInWithEmailAndPassword,
+//   signInWithPopup,
+//   signOut,
+// } from "firebase/auth";
+// import {
+//   getFirestore,
+//   collection,
+//   addDoc,
+//   query,
+//   orderBy,
+//   limit,
+//   onSnapshot,
+//   setDoc,
+//   updateDoc,
+//   doc,
+//   serverTimestamp,
+// } from "firebase/firestore";
+// import {
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+//   getDownloadURL,
+// } from "firebase/storage";
+// import { getMessaging, getToken, onMessage } from "firebase/messaging";
+// import { getPerformance } from "firebase/performance";
 
-import { getFirebaseConfig } from "./firebase-config.js";
+// import { getFirebaseConfig } from "./firebase-config.js";
 
-const firebaseAppConfig = getFirebaseConfig();
-initializeApp(firebaseAppConfig);
+// const firebaseAppConfig = getFirebaseConfig();
+// initializeApp(firebaseAppConfig);
